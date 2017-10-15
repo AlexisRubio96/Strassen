dcfe3251fdaccd39dcf480f2a2793ae79642d3d9
AlexisRubio96 <jarss96@hotmail.com>
2017-10-15 18:04:51 -0500

    Mejora en el formato del código

--- Strassen.py
+++ Strassen.py
@@ -1,78 +1,83 @@
+"""
+Ernesto Aelejandro Cervantes Villa  A01370793
+Jorge Alexis Rubio Sumano   A01372074
+
+Strassen Algorithm Implementation
+"""
 import csv
-#***********************************************************************************************************************
-# Se trabaja con la primer matriz --> Matriz_A_16_2_4.csv
-#***********************************************************************************************************************
 
-with open('Matriz_A_16_2_4.csv','r') as archivo:
-
-# Lectura de archivo
-# Se abre el archivo
-    r = csv.reader(archivo)
-
-# Se lee el archivo completo para conocer la dimension de la matriz
-# Se crea la matriz
-# Esto se puede optimizar si solo se lee la primer linea, debido a que la matriz es
-# cuadrada conociendo el len() del primer renglon podemos saber cuantos renglones y columnas necesitamos
-    for x in r:
-        matrizA = []
-        for i in range(len(x)):
-            matrizA.append([])
-            for j in range(len(x)):
-                matrizA[i].append(None)
-
-
-archivo.close
-
-# Matriz vacia
-print matrizA
-
-# Longitud de la matriz nxn = longitud
-# Esta longitud sirve para las dos matrices que se multiplicaran
-longitud = len(matrizA[0])
-
-# Se vuelve a abrir el archivo para llenar la matriz
-with open('Matriz_A_16_2_4.csv','r') as archivo:
-    r = csv.reader(archivo)
-    i = 0
-
-    # Se llena la matriz A
-    for x in r:
-        for j in range(longitud):
-            matrizA[i][j] = int(x[j])
-        i+=1
-
-archivo.close
-# Matriz llena
-print matrizA
-
-# Se crea la matriz B, tiene la misma dimension que la matriz A, solo hay que cambiar valores
-matrizB = matrizA
+"""
+****************************File reading************************************
+This method takes care of the csv file reading.
+Returns a multidimensional array of size [n][n], i.e. a matrix nxn
+"""
+def matrix_reader(file):
+    # File read
+    # File is open
+    with open(file, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        # Matrix creation
+        matrix = []
+        row_number = 0
+        for row in reader:
+            matrix.append([])
+            # rowxrow = nxn
+            for i in range(len(row)):
+                matrix[row_number].append(int(row[i]))
+                # print row[i]
+                # for j in range(len(row)):
+                #   matrix[row_number][j]
+            row_number += 1
+    csv_file.close()
+    return matrix
 
 
-#***********************************************************************************************************************
-# Se trabaja con la segunda matriz --> Matriz_B_16_2_4.csv
-#***********************************************************************************************************************
+"""
+****************************Matrix multiplication type 1**************************************
+This resolves the first part of the activity(III.1): matrix multiplication defined in any linear algebra text book.
+Take both global matrices A and B, and the result matrix of text book.
+"""
 
-with open('Matriz_B_16_2_4.csv','r') as archivo:
-    r = csv.reader(archivo)
-    i = 0
+def text_book_matrix_multiplication():
+    global matrixA, matrixB, result_matrix_tb
+    lenght = len(matrixA)
+    for i in range(lenght):
+        for j in range(lenght):
+            for k in range(lenght):
+                result_matrix_tb[i][k] = result_matrix_tb[i][k] + (matrixA[i][j] * matrixB[j][k])
 
-    # Se llena la matriz B
-    for x in r:
-        for j in range(longitud):
-            matrizB[i][j] = int(x[j])
-        i+=1
-
-# Se tienen listas las matrices a multiplicas --> matrizA y matrizB
-print matrizB
-
-# Se crea la matriz C, tiene la misma dimension que la matriz A, matriz B. La matriz C es nuestra matriz resultante
-matrizC = matrizB
-
-for i in range(longitud):
-    for j in range(longitud):
-        for k in range(longitud):
-            matrizC[i][k] = matrizC[i][k] + (matrizA[i][j] * matrizB[j][k])
+    print result_matrix_tb
 
 
-print matrizC
+"""
+****************************Matrix multiplication type 2**************************************
+This resolves the second part of the activity(III.2): implement the Strassen algorithm for the multiplication of matrices of order n.
+"""
+def strassen_algorithm():
+    print result_matrix_tb
+
+
+"""
+Global variables
+"""
+result_matrix_strassen = []
+result_matrix_tb = []
+matrixA = []
+matrixB = []
+
+
+def main():
+    global matrixA, matrixB
+    """
+    ##########Implementar al final###############
+    input_matrixA = input("Enter the csv file name of the MatrixA: ")
+    input_matrixB = input("Enter the csv file name of the MatrixB: ")
+    input_matrixRes = input("Enter the csv file to save the result: ")
+    """
+    matrixA = matrix_reader('Matriz_A_16_2_4.csv')
+    print matrixA
+    matrixB = matrix_reader('Matriz_B_16_2_4.csv')
+    print matrixB
+
+
+main()
