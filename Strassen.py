4ba1fab8a39298a7c3725e6c65e0b80bde33a169
AlexisRubio96 <jarss96@hotmail.com>
2017-10-16 10:19:34 -0500

    Matriz dividida en cuartos

--- Strassen.py
+++ Strassen.py
@@ -10,6 +10,7 @@ import csv
 ****************************File reading************************************
 This method takes care of the csv file reading.
 Returns a multidimensional array of size [n][n], i.e. a matrix nxn
+matrix_reader(file): file = csv file to be read
 """
 def matrix_reader(file):
     # File read
@@ -67,8 +68,56 @@ def text_book_matrix_multiplication():
 ****************************Matrix multiplication type 2**************************************
 This resolves the second part of the activity(III.2): implement the Strassen algorithm for the multiplication of matrices of order n.
 """
-def strassen_algorithm():
-    print result_matrix_strassen
+
+def divide_matrix(temp_matrix, quarter):
+    matrix_quarter = []
+    length = len(temp_matrix)/2
+    for row in range(length):
+        matrix_quarter.append([])
+        if quarter == 1:
+            matrix_quarter[row] = temp_matrix[row][:len(temp_matrix)/2]
+        if quarter == 2:
+            matrix_quarter[row] = temp_matrix[row][len(temp_matrix)/2:]
+        if quarter == 3:
+            matrix_quarter[row] = temp_matrix[(length)+row][:len(temp_matrix)/2]
+        if quarter == 4:
+            matrix_quarter[row] = temp_matrix[(length)+row][len(temp_matrix)/2:]
+    return matrix_quarter
+
+def strassen_algorithm(temp_matrix_a, temp_matrix_b):
+    length = len(temp_matrix_a)
+
+# base case: 1x1 matrix
+    if length == 1:
+        d = [[0]]
+        d[0][0] = temp_matrix_a[0][0] * temp_matrix_b[0][0]
+        return d
+    else:
+        #Matrices quarters
+        a11 = divide_matrix(temp_matrix_a, 1)
+        a12 = divide_matrix(temp_matrix_a, 2)
+        a21 = divide_matrix(temp_matrix_a, 3)
+        a22 = divide_matrix(temp_matrix_a, 4)
+        b11 = divide_matrix(temp_matrix_b, 1)
+        b12 = divide_matrix(temp_matrix_b, 2)
+        b21 = divide_matrix(temp_matrix_b, 3)
+        b22 = divide_matrix(temp_matrix_b, 4)
+    return
+    """
+
+        #Base case matrix 1x1
+        if length == 1:
+            temp_matrix[0][0] = temp_matrix_a[0][0]*temp_matrix_b[0][0]
+        else:
+            # Fill temporal matrix with 0
+            temp_matrix = []
+            for row in range(length):
+                temp_matrix.append([])
+                for column in range(length):
+                    temp_matrix[row].append(0)
+            print temp_matrix
+            length_submatrix = length/2
+    """
 
 
 """
@@ -83,22 +132,26 @@ matrixB = []
 
 
 def main():
+
     global matrixA, matrixB
     """
     ##########Implementar al final###############
     input_matrixA = input("Enter the csv file name of the MatrixA: ")
     input_matrixB = input("Enter the csv file name of the MatrixB: ")
-    """
     input_matrixRes = raw_input("Enter the route for the new csv file to save the result: ")
+    """
 
-    matrixA = matrix_reader('Matriz_A_16_2_4.csv')
+
+    matrixA = matrix_reader('matriz_prueba_A.csv')
     print matrixA
-    matrixB = matrix_reader('Matriz_B_16_2_4.csv')
+    matrixB = matrix_reader('matriz_prueba_B.csv')
     print matrixB
     text_book_matrix_multiplication()
     print result_matrix_tb
     print "Number of scalar multiplications in the text book method: " + str(number_tb_multiplication)
-    file_writing(input_matrixRes)
+    #file_writing(input_matrixRes)
+    print strassen_algorithm(matrixA,matrixB)
+
 
 
 main()
